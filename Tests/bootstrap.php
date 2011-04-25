2f6209e33a12d60c84a52cf969e7d13a62280d52
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-04-25 02:02:36 +0200

    added a phpunit.xml.dist and bootstrap

---
+++ Tests/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+
+$loader = new UniversalClassLoader();
+$loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
+$loader->registerNamespace('Doctrine', $_SERVER['DOCTRINE']);
+$loader->register();
+
+spl_autoload_register(function($class)
+{
+    if (0 === strpos($class, 'FOS\\RestBundle\\')) {
+        $path = implode('/', array_slice(explode('\\', $class), 2)).'.php';
+        require_once __DIR__.'/../'.$path;
+        return true;
+    }
+});
