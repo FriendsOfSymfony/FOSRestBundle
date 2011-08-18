339eddb30be3d828e753a715efc3d71534fb528b
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-08-18 07:44:57 +0200

    updated license header

--- EventListener/BodyListener.php
+++ EventListener/BodyListener.php
@@ -1,21 +1,19 @@
 <?php
 
+/*
+ * This file is part of the FOSRestBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace FOS\RestBundle\EventListener;
 
 use Symfony\Component\HttpFoundation\ParameterBag,
     Symfony\Component\HttpKernel\Event\GetResponseEvent,
     Symfony\Component\DependencyInjection\ContainerAware;
-
-/*
- * This file is part of the FOSRestBundle
- *
- * (c) Lukas Kahwe Smith <smith@pooteeweet.org>
- * (c) Konstantin Kudryashov <ever.zet@gmail.com>
- * (c) Bulat Shakirzyanov <avalanche123>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
 
 /**
  * This listener handles Request body decoding.
--- EventListener/FormatListener.php
+++ EventListener/FormatListener.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the FOSRestBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace FOS\RestBundle\EventListener;
 
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent,
@@ -8,17 +17,6 @@ use Symfony\Component\HttpKernel\Event\FilterControllerEvent,
     Symfony\Component\HttpKernel\HttpKernelInterface;
 
 use FOS\RestBundle\Response\Codes;
-
-/*
- * This file is part of the FOSRestBundle
- *
- * (c) Lukas Kahwe Smith <smith@pooteeweet.org>
- * (c) Konstantin Kudryashov <ever.zet@gmail.com>
- * (c) Bulat Shakirzyanov <avalanche123>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
 
 /**
  * This listener handles Accept header format negotiations.
