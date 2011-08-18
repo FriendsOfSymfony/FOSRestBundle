6d75a001b281bb99543d27f0af0d13cbacdc79ad
Johannes Schmitt <schmittjoh@gmail.com>
2011-08-18 07:40:21 +0200

    added license headers

--- Request/ContentNegotiator.php
+++ Request/ContentNegotiator.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+ * This file is part of the FOSRestBundle
+ *
+ * (c) Lukas Kahwe Smith <smith@pooteeweet.org>
+ * (c) Konstantin Kudryashov <ever.zet@gmail.com>
+ * (c) Bulat Shakirzyanov <avalanche123>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace FOS\RestBundle\Request;
 
 use Symfony\Component\HttpFoundation\Request;
