e600fdbfbac5cbb8dae960582bc9455ee39f741a
Tarjei Huse <tarjei@scanmine.com>
2011-09-07 12:23:35 +0200

    Cleanups as per willdurands comments

--- Resources/docs/examples/RssHandler.php
+++ Resources/docs/examples/RssHandler.php
@@ -4,11 +4,10 @@ namespace FOS\RestBundle\Examples;
 
 use FOS\RestBundle\View\View,
     FOS\RestBundle\View\ViewHandler,
+    Symfony\Bridge\Monolog\Logger,
     Symfony\Component\HttpFoundation\Request,
     Symfony\Component\HttpFoundation\Response;
     ;
-
-
 /**
  * This is an example ViewHandler for converting i to RSS.
  * It also shows how to handle exceptions within the ViewHandler so that the 
@@ -22,75 +21,77 @@ use FOS\RestBundle\View\View,
     arguments:
       logger: @logger
  */
-class RssHandler {
+class RssHandler
+{
 
-  private $log;
+    private $log;
 
-  public function __construct(
-    \Symfony\Bridge\Monolog\Logger $logger
-      ) {
-    $this->log = $logger;
-  }
-
-  /**
-   * Converts the viewdata to a RSS feed. Modify to suit your datastructure.
-   *
-   */
-  public function createResponse(ViewHandler $handler,View $view,Request $request) {
-    try {
-      return new Response($this->createFeed($view->getData()), 200, $view->getHeaders());
-    } catch (Exception $e) {
-      return $this->handleException($e, $view->getHeaders());
-    } catch (\Zend_Feed_Exception $e) {
-      return $this->handleException($e, $view->getHeaders());
+    public function __construct(Logger $logger) 
+    {
+        $this->log = $logger;
     }
-  }
-  protected function handleException($e, $headers) {
-    $this->log->addError($e);
-    return new Response($e->getMessage() . "<br/><pre>" . $e->getStackTrace()
-          . "</pre>", 500, $headers );
-  }
 
-  protected function createFeed($data, $format = "rss") {
-    $feed = new \Zend_Feed_Writer_Feed;
-    $feed->setTitle($data['title']);
-    $feed->setLink($data['link']);
-    $feed->setFeedLink($data['link'], 'rss');
-    $feed->addAuthor(array(
-          'name'  => 'ZeroCMS',
-          'email' => 'email!',
-          ));
-    $feed->setDateModified(time());
-    $feed->setDescription("RSS feed from query");
     /**
-     * Add one or more entries. Note that entries must
-     * be manually added once created.
+     * Converts the viewdata to a RSS feed. Modify to suit your datastructure.
+     * @return Response
      */
-    foreach ($data['documents'] as $document) {
-      $entry = $feed->createEntry();
-
-        $entry->setTitle($document['title']);
-        $entry->setLink($document['url']);
-        $entry->addAuthor(array(
-              'name'  => $document['author'],
-              //'email' => '',
-              //'uri'   => '',
-              ));
-
-        $entry->setDateModified($document['dateUpdated']->getTimestamp());
-        $entry->setDateCreated($document['dateCreated']->getTimestamp());
-        if (isset($document['summary'])) {
-          $entry->setDescription($document['summary']);
+    public function createResponse(ViewHandler $handler,View $view,Request $request)
+    {
+        try {
+            return new Response($this->createFeed($view->getData()), 200, $view->getHeaders());
+        } catch (\Exception $e) {
+            return $this->handleException($e, $view->getHeaders());
+        } catch (\Zend_Feed_Exception $e) {
+            return $this->handleException($e, $view->getHeaders());
         }
-        $entry->setContent($document['body'] );
-        $feed->addEntry($entry);
+    }
+    
+    protected function handleException($e, $headers)
+    {
+        $this->log->addError($e);
+        return new Response(
+                sprintf("%s:<br/><pre>%s</pre>", $e->getStackTrace(),
+                    $e->getMessage(), 500, $headers );
     }
     /**
-     * Render the resulting feed to Atom 1.0 and assign to $out.
-     * You can substitute "atom" with "rss" to generate an RSS 2.0 feed.
+     * @param $data array
+     * @param format string, either rss or atom
      */
-    return $feed->export($format);
-  }
+    protected function createFeed($data, $format = "rss")
+    {
+        $feed = new \Zend_Feed_Writer_Feed;
+        $feed->setTitle($data['title']);
+        $feed->setLink($data['link']);
+        $feed->setFeedLink($data['link'], 'rss');
+        $feed->addAuthor(array(
+                    'name'  => 'ZeroCMS',
+                    'email' => 'email!',
+                    ));
+        $feed->setDateModified(time());
+        $feed->setDescription("RSS feed from query");
+        // Add one or more entries. Note that entries must
+        // be manually added once created.
+        foreach ($data['documents'] as $document) {
+            $entry = $feed->createEntry();
+
+            $entry->setTitle($document['title']);
+            $entry->setLink($document['url']);
+            $entry->addAuthor(array(
+                        'name'  => $document['author'],
+                        //'email' => '',
+                        //'uri'   => '',
+                        ));
+
+            $entry->setDateModified($document['dateUpdated']->getTimestamp());
+            $entry->setDateCreated($document['dateCreated']->getTimestamp());
+            if (isset($document['summary'])) {
+                $entry->setDescription($document['summary']);
+            }
+            $entry->setContent($document['body'] );
+            $feed->addEntry($entry);
+        }
+        return $feed->export($format);
+    }
 
 
 }
