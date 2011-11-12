434a64c60853502acbedf0b940a8d7cebcde8f71
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-11-12 12:17:40 +0100

    added file headers

--- vendor/vendors.php
+++ vendor/vendors.php
@@ -1,6 +1,14 @@
 #!/usr/bin/env php
 <?php
 
+/*
+ * This file is part of the FOSRestBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 set_time_limit(0);
 
 $vendorDir = __DIR__;
