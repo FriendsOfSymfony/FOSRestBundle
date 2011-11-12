7065695f05d8288baeec3ff413583dbb30ebcba9
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-11-12 10:45:57 +0100

    travis-ci

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,32 +1,7 @@
 <?php
 
-require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
-
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
-$loader->registerNamespace('Doctrine', $_SERVER['DOCTRINE']);
-$loader->registerNamespace('Sensio\\Bundle\\FrameworkExtraBundle\\', __DIR__.'/../../..');
-$loader->register();
-
-spl_autoload_register(function($class)
-{
-    if (0 === strpos($class, 'FOS\\RestBundle\\')) {
-        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
-        if (!stream_resolve_include_path($path)) {
-            return false;
-        }
-        require_once $path;
-        return true;
-    }
-});
-
-use Doctrine\Common\Annotations\AnnotationRegistry;
-AnnotationRegistry::registerLoader(function($class) {
-    if (strpos($class, 'FOS\RestBundle\Controller\Annotations\\') === 0) {
-        $path = __DIR__.'/../'.str_replace('\\', '/', substr($class, strlen('FOS\RestBundle\\')))   .'.php';
-        require_once $path;
-    }
-    return class_exists($class, false);
-});
+if (file_exists($file = __DIR__.'/autoload.php')) {
+    require_once $file;
+} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
+    require_once $file;
+}
---
+++ vendor/vendors.php
@@ -0,0 +1,25 @@
+#!/usr/bin/env php
+<?php
+
+set_time_limit(0);
+
+$vendorDir = __DIR__;
+$deps = array(
+    array('symfony', 'http://github.com/symfony/symfony', 'v2.0.5'),
+    array('doctrine-common', 'http://github.com/doctrine/common.git', 'origin/master'),
+    array('JMS/SerializerBundle', 'http://github.com/schmittjoh/JMSSerializerBundle', 'origin/master'),
+    array('Sensio/Bundle/FrameworkExtraBundle', 'http://github.com/sensio/SensioFrameworkExtraBundle', 'origin/master'),
+);
+
+foreach ($deps as $dep) {
+    list($name, $url, $rev) = $dep;
+
+    echo "> Installing/Updating $name\n";
+
+    $installDir = $vendorDir.'/'.$name;
+    if (!is_dir($installDir)) {
+        system(sprintf('git clone %s %s', escapeshellarg($url), escapeshellarg($installDir)));
+    }
+
+    system(sprintf('cd %s && git fetch origin && git reset --hard %s', escapeshellarg($installDir), escapeshellarg($rev)));
+}
