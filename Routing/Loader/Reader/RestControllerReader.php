01a4cd86e39669ed40b420f2b7e7af495fde56c2
everzet <ever.zet@gmail.com>
2011-12-28 20:36:00 +0100

    implemented controller reader

---
+++ Routing/Loader/Reader/RestControllerReader.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace FOS\RestBundle\Routing\Loader\Reader;
+
+use Symfony\Component\Config\Resource\FileResource;
+use Doctrine\Common\Annotations\Reader;
+
+/**
+ * REST controller reader.
+ */
+class RestControllerReader
+{
+    private $actionReader;
+    private $annotationReader;
+
+    /**
+     * Initializes controller reader.
+     *
+     * @param RestActionReader $actionReader     action reader
+     * @param Reader           $annotationReader annotation reader
+     */
+    public function __construct(RestActionReader $actionReader, Reader $annotationReader)
+    {
+        $this->actionReader     = $actionReader;
+        $this->annotationReader = $annotationReader;
+    }
+
+    /**
+     * Returns action reader.
+     *
+     * @return RestActionReader
+     */
+    public function getActionReader()
+    {
+        return $this->actionReader;
+    }
+
+    /**
+     * Reads controller routes.
+     *
+     * @param string $class
+     *
+     * @return RestRouteCollection
+     */
+    public function read($class)
+    {
+        $reflection = new \ReflectionClass($class);
+        $collection = new RestRouteCollection();
+        $collection->addResource(new FileResource($class->getFileName()));
+
+        // read prefix annotation
+        if ($annotation = $this->readClassAnnotation($reflection, 'Prefix')) {
+            $this->actionReader->setRoutePrefix($annotation->value);
+        }
+
+        // read name-prefix annotation
+        if ($annotation = $this->readClassAnnotation($reflection, 'NamePrefix')) {
+            $this->actionReader->setNamePrefix($annotation->value);
+        }
+
+        // trim '/' at the start of the prefix
+        if ('/' === substr($prefix = $this->actionReader->getRoutePrefix(), 0, 1)) {
+            $this->actionReader->setRoutePrefix(substr($prefix, 1));
+        }
+
+        // read action routes into collection
+        foreach ($class->getMethods(\ReflectionMethod::IS_PUBLIC) as $method) {
+            $this->actionReader->read($collection, $method);
+        }
+
+        return $collection;
+    }
+
+    /**
+     * Reads
+     *
+     * @param ReflectionClass $reflection     controller class
+     * @param string          $annotationName annotation name
+     *
+     * @return Annotation|null
+     */
+    private function readClassAnnotation(\ReflectionClass $reflection, $annotationName)
+    {
+        $annotationClass = "FOS\\RestBundle\\Controller\\Annotations\\$annotationName";
+
+        if ($annotation = $this->annotationReader->getClassAnnotation($reflection, $annotationClass)) {
+            return $annotation;
+        }
+    }
+}
