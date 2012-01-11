82e354c749ff4d53c8844d7c1e98ccc0ff032189
lsmith77 <smith@pooteeweet.org>
2012-01-11 16:21:09 +0100

    cosmetics

--- EventListener/ViewResponseListener.php
+++ EventListener/ViewResponseListener.php
@@ -69,11 +69,10 @@ class ViewResponseListener extends TemplateListener
     public function onKernelView(GetResponseForControllerResultEvent $event)
     {
         $request = $event->getRequest();
+        $configuration = $request->attributes->get('_view');
 
         $view = $event->getControllerResult();
-        $configuration = $request->attributes->get('_view');
         if (!$view instanceOf View) {
-
             if (!$configuration && !$this->container->getParameter('fos_rest.view_response_listener.force_view')) {
                 return;
             }
