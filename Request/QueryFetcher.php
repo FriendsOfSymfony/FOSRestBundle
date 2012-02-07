80d0da4100a2c0afd6c5a363b9b992112aeee9dc
lsmith77 <smith@pooteeweet.org>
2012-02-07 20:56:03 +0100

    added support for controllers as a service, moved logic out of the constructor

--- Request/QueryFetcher.php
+++ Request/QueryFetcher.php
@@ -11,6 +11,7 @@
 
 namespace FOS\RestBundle\Request;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -21,9 +22,14 @@ use Symfony\Component\HttpFoundation\Request;
 class QueryFetcher
 {
     /**
-     * @var array
+     * @var ContainerInterface
      */
-    private $params;
+    private $container;
+
+    /**
+     * @var QueryParamReader
+     */
+    private $queryParamReader;
 
     /**
      * @var Request
@@ -31,21 +37,22 @@ class QueryFetcher
     private $request;
 
     /**
+     * @var array
+     */
+    private $params;
+
+
+    /**
      * Initializes fetcher.
      *
-     * @param Request              $request          Active request
-     * @param RestQueryParamReader $queryParamReader Query param reader
+     * @param ContainerInterface $container       Container
+     * @param Request           $request          Active request
+     * @param QueryParamReader  $queryParamReader Query param reader
      */
-    public function __construct(Request $request, QueryParamReader $queryParamReader)
+    public function __construct(ContainerInterface $container, QueryParamReader $queryParamReader, Request $request)
     {
-        if (null === $request->attributes->get('_controller')) {
-            throw new \InvalidArgumentException("No _controller for request.");
-        }
-
-        // todo: for now Controller::Method notation is assumed
-        list($class, $method) = explode('::', $request->attributes->get('_controller'));
-
-        $this->params = $queryParamReader->read(new \ReflectionClass($class), $method);
+        $this->container = $container;
+        $this->queryParamReader = $queryParamReader;
         $this->request = $request;
     }
 
@@ -53,12 +60,32 @@ class QueryFetcher
      * Get a validated query parameter.
      *
      * @param string $name    Name of the query parameter
-     * @param mixed  $default Default variable if the parameter is not set or doesn't match the requirements
      *
      * @return mixed Value of the parameter.
      */
-    public function getParameter($name, $default)
+    public function getParameter($name)
     {
+        if (!isset($this->params)) {
+            $_controller = $this->request->attributes->get('_controller');
+
+            if (null === $_controller) {
+                throw new \InvalidArgumentException('No _controller for request.');
+            }
+
+            if (false !== strpos($_controller, '::')) {
+                list($class, $method) = explode('::', $this->request->attributes->get('_controller'));
+            } else {
+                list($controller, $method) = explode(':', $_controller);
+                if (!$this->container->has($controller)) {
+                    throw new \InvalidArgumentException('Controller service for request not available: '.$controller);
+                }
+                $controller = $this->container->get($controller);
+                $class = get_class($controller);
+            }
+
+            $this->params = $this->queryParamReader->read(new \ReflectionClass($class), $method);
+        }
+
         if (!isset($this->params[$name])) {
             throw new \InvalidArgumentException(sprintf("No @QueryParam configuration for parameter '%s'.", $name));
         }
