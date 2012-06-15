9e82f0bffddbffcfc626a3a017e7b66d7ecb8787
Antoni Orfin <a.orfin@imagin.com.pl>
2012-06-15 22:18:18 +0200

    Added DataTransferObject.

---
+++ Request/DataTransferObject.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the FOSRestBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\RestBundle\Request;
+
+/**
+ * DataTransferObject
+ * 
+ * All objects which you want to deserialize from the Request content 
+ * must extend DataTransferObject.
+ * 
+ * Instances should accept empty constructor.
+ * 
+ * @author Antoni Orfin <a.orfin@imagin.com.pl>
+ * @copyright (c) 2012  IMAGIN IT Marcin Engelmann
+ */
+abstract class DataTransferObject
+{
+}
