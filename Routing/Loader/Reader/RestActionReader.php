FriendsOfSymfony/FOSRestBundle

commit 7a6f101e4e101c2c183c4dc346747dff6e265d28

minor tweak

Routing/Loader/Reader/RestActionReader.php
@@ -159,6 +159,7 @@ class RestActionReader
         }
 
         $routeName = $httpMethod.$this->generateRouteName($resources);
+
         $urlParts  = $this->generateUrlParts($resources, $arguments, $httpMethod);
 
         // if passed method is not valid HTTP method then it's either
@@ -342,7 +343,7 @@ class RestActionReader
                     $urlParts[] = '{'.$arguments[$i]->getName().'}';
                 }
             } elseif (null !== $resource) {
-                if (!in_array($httpMethod, $this->availableHTTPMethods)) {
+                if (0 === count($arguments)) {
                     $urlParts[] = Pluralization::pluralize(strtolower($resource));
                 } else {
                     $urlParts[] = strtolower($resource);

commit 93564df46d4f32b9031afd7f9cc57c0a71a6fc04

Do not pluralize all actions with 0 arguments by definition
Fixes a regression introduced in 7a6f101e4e101c2c183c4dc346747dff6e265d28.

Routing/Loader/Reader/RestActionReader.php
@@ -342,7 +342,9 @@ class RestActionReader
                     $urlParts[] = '{'.$arguments[$i]->getName().'}';
                 }
             } elseif (null !== $resource) {
-                if (0 === count($arguments) || 'new' === $httpMethod) {
+                if ((0 === count($arguments) && !in_array($httpMethod, $this->availableHTTPMethods))
+                    || 'new' === $httpMethod
+                ) {
                     $urlParts[] = Pluralization::pluralize(strtolower($resource));
                 } else {
                     $urlParts[] = strtolower($resource);