1c9dceec14bb722984dd0df105ce01f87304f8cc
Nils Wisiol <nils.wisiol@dothiv.org>
2013-05-04 11:46:26 -0400

    reverted unrelated changes from last commit

--- View/ViewHandler.php
+++ View/ViewHandler.php
@@ -11,8 +11,10 @@
 
 namespace FOS\RestBundle\View;
 
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use JMS\Serializer\Serializer;
+use JMS\Serializer\SerializationContext;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\HttpException;
@@ -108,7 +110,7 @@ class ViewHandler extends ContainerAware implements ViewHandlerInterface
     /**
      * Registers a custom handler
      *
-     * The handler must have the following signature: handler($viewObject, $request, $response)
+     * The handler must have the following signature: handler(ViewHandler $viewHandler, View $view, Request $request, $format)
      * It can use the public methods of this class to retrieve the needed data and return a
      * Response object ready to be sent.
      *
@@ -138,7 +140,7 @@ class ViewHandler extends ContainerAware implements ViewHandlerInterface
      */
     protected function getStatusCode(View $view, $content = null)
     {
-        if (null !== ($code = $view->getStatusCode())) {
+        if (200 !== ($code = $view->getStatusCode())) {
             return $code;
         }
 
@@ -189,27 +191,35 @@ class ViewHandler extends ContainerAware implements ViewHandlerInterface
      */
     protected function getSerializer(View $view = null)
     {
-        $serializer = $this->container->get('fos_rest.serializer');
+        return $this->container->get('fos_rest.serializer');
+    }
 
-        if ($view && $serializer instanceof Serializer) {
-            $groups = $view->getSerializerGroups() ?: $this->container->getParameter('fos_rest.serializer.exclusion_strategy.groups');
-            $version = $view->getSerializerVersion() ?: $this->container->getParameter('fos_rest.serializer.exclusion_strategy.version');
+    /**
+     * Gets or creates a JMS\Serializer\SerializationContext and initializes it with
+     * the view exclusion strategies, groups & versions if a new context is created
+     *
+     * @param View $view
+     *
+     * @return SerializationContext
+     */
+    public function getSerializationContext(View $view)
+    {
+        $context = $view->getSerializationContext();
+        if (null === $context) {
+            $context = new SerializationContext();
 
-            if ($groups && $version) {
-                $serializer->setExclusionStrategy(new GroupsVersionExclusionStrategy($groups, $version));
-            } elseif ($groups) {
-                $serializer->setGroups($groups);
-            } elseif ($version) {
-                $serializer->setVersion($version);
+            $groups = $this->container->getParameter('fos_rest.serializer.exclusion_strategy.groups');
+            if ($groups) {
+                $context->setGroups($groups);
             }
 
-            $callback = $view->getSerializerCallback();
-            if ($callback) {
-                call_user_func($callback, $this, $serializer);
+            $version = $this->container->getParameter('fos_rest.serializer.exclusion_strategy.version');
+            if ($version) {
+                $context->setVersion($version);
             }
         }
 
-        return $serializer;
+        return $context;
     }
 
     /**
