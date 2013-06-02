3774f854669013c691e108cef80a1a2f4304438b
Tyler Stroud <tyler@tylerstroud.com>
2013-06-01 18:07:40 -0700

    Allow the serializer context for the request body converter to be configured

--- Request/RequestBodyParamConverter.php
+++ Request/RequestBodyParamConverter.php
@@ -14,10 +14,13 @@ namespace FOS\RestBundle\Request;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Serializer\Exception\Exception as SymfonySerializerException;
+use Symfony\Component\Serializer\SerializerInterface as SymfonySerializerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ConfigurationInterface;
 use JMS\Serializer\Exception\UnsupportedFormatException;
 use JMS\Serializer\Exception\Exception as JMSSerializerException;
+use JMS\Serializer\DeserializationContext;
+use JMS\Serializer\SerializerInterface;
 use FOS\Rest\Util\Codes;
 
 /**
@@ -43,11 +46,25 @@ class RequestBodyParamConverter implements ParamConverterInterface
      */
     public function apply(Request $request, ConfigurationInterface $configuration)
     {
+        $context = null;
+        $options = $configuration->getOptions();
+        if (isset($options['serializationContext']) && is_array($options['serializationContext'])) {
+            if ($this->serializer instanceof SerializerInterface) {
+                $context = $this->configureDeserializationContext(
+                    $this->getDeserializationContext(),
+                    $options['serializationContext']
+                );
+            } elseif ($this->serializer instanceof SymfonySerializerInterface) {
+                $context = $options['serializationContext'];
+            }
+        }
+
         try {
             $object = $this->serializer->deserialize(
                 $request->getContent(),
                 $configuration->getClass(),
-                $request->getContentType()
+                $request->getContentType(),
+                $context
             );
         } catch (UnsupportedFormatException $e) {
             throw new HttpException(Codes::HTTP_UNSUPPORTED_MEDIA_TYPE, $e->getMessage());
@@ -68,5 +85,31 @@ class RequestBodyParamConverter implements ParamConverterInterface
     public function supports(ConfigurationInterface $configuration)
     {
         return null !== $configuration->getClass();
+    }
+
+    /**
+     * @return DeserializationContext
+     */
+    protected function getDeserializationContext()
+    {
+        return DeserializationContext::create();
+    }
+
+    /**
+     * @param DeserializationContext $context
+     * @param array                  $options
+     *
+     * @return DeserializationContext
+     */
+    protected function configureDeserializationContext(DeserializationContext $context, array $options)
+    {
+        if (isset($options['groups'])) {
+            $context->setGroups($options['groups']);
+        }
+        if (isset($options['version'])) {
+            $context->setVersion($options['version']);
+        }
+
+        return $context;
     }
 }
