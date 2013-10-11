bc0f820715625d0cabc5ee0894a24a9df4403658
Lukas Kahwe Smith <smith@pooteeweet.org>
2013-10-11 15:45:22 +0200

    use the core XmlEncoder class inside XmlDecoder

--- Decoder/XmlDecoder.php
+++ Decoder/XmlDecoder.php
@@ -11,6 +11,8 @@
 
 namespace FOS\RestBundle\Decoder;
 
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+
 /**
  * Decodes XML data
  *
@@ -20,78 +22,18 @@ namespace FOS\RestBundle\Decoder;
  */
 class XmlDecoder implements DecoderInterface
 {
+    private $encoder;
+
+    public function __construct()
+    {
+        $this->encoder = new XmlEncoder();
+    }
+
     /**
      * {@inheritdoc}
      */
     public function decode($data)
     {
-        $xml = @simplexml_load_string($data);
-        if (!$xml) {
-            return;
-        }
-
-        if (!$xml->count()) {
-            if (!$xml->attributes()) {
-                return (string) $xml;
-            }
-            $data = array();
-            foreach ($xml->attributes() as $attrkey => $attr) {
-                $data['@'.$attrkey] = (string) $attr;
-            }
-            $data['#'] = (string) $xml;
-
-            return $data;
-        }
-
-        return $this->parseXml($xml);
-    }
-
-    /**
-     * Parse the input SimpleXmlElement into an array
-     *
-     * @param \SimpleXmlElement $node xml to parse
-     * @return array
-     */
-    private function parseXml(\SimpleXmlElement $node)
-    {
-        $data = array();
-        if ($node->attributes()) {
-            foreach ($node->attributes() as $attrkey => $attr) {
-                $data['@'.$attrkey] = (string) $attr;
-            }
-        }
-        foreach ($node->children() as $key => $subnode) {
-            if ($subnode->count()) {
-                $value = $this->parseXml($subnode);
-            } elseif ($subnode->attributes()) {
-                $value = array();
-                foreach ($subnode->attributes() as $attrkey => $attr) {
-                    $value['@'.$attrkey] = (string) $attr;
-                }
-                $value['#'] = (string) $subnode;
-            } else {
-                $value = (string) $subnode;
-            }
-
-            if ($key === 'item') {
-                if (isset($value['@key'])) {
-                    $data[(string)$value['@key']] = $value['#'];
-                } elseif (isset($data['item'])) {
-                    $tmp = $data['item'];
-                    unset($data['item']);
-                    $data[] = $tmp;
-                    $data[] = $value;
-                }
-            } elseif (array_key_exists($key, $data)) {
-                if ((false === is_array($data[$key]))  || (false === isset($data[$key][0]))) {
-                    $data[$key] = array($data[$key]);
-                }
-                $data[$key][] = $value;
-            } else {
-                $data[$key] = $value;
-            }
-        }
-
-        return $data;
+        return $this->encoder->decode($data, 'xml');
     }
 }
