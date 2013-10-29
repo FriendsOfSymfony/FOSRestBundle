d535ce2f7666e2166455d1a561956ce946b027bf
= <=>
2013-10-29 21:37:50 +0000

    refactored this to be a bit tidier and more efficient

--- EventListener/BodyListener.php
+++ EventListener/BodyListener.php
@@ -62,12 +62,13 @@ class BodyListener
             }
 
             $decoder = $this->decoderProvider->getDecoder($format);
-
-            $data = $decoder->decode($request->getContent(), $format);
-            if (is_array($data)) {
-                $request->request = new ParameterBag($data);
-            } else {
-                if (strlen($request->getContent())) {
+            $content = $request->getContent();
+            
+			if (strlen($content)) {
+				$data = $decoder->decode($content, $format);
+				if (is_array($data)) {
+					$request->request = new ParameterBag($data);
+				} else {                
                     throw new BadRequestHttpException('Invalid ' . $format . ' message recieved');
                 }
             }
