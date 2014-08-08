025986e0f316da1225a0e93725056a112c4e52ce
Pablo Godel <pgodel@gmail.com>
2014-08-07 23:12:43 -0400

    Include config in exception message
    Added the configuration needed to disable view annocations.

--- DependencyInjection/Compiler/ConfigurationCheckPass.php
+++ DependencyInjection/Compiler/ConfigurationCheckPass.php
@@ -24,7 +24,7 @@ class ConfigurationCheckPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         if ($container->has('sensio_framework_extra.view.listener') && $container->has('fos_rest.view_response_listener')) {
-            throw new \RuntimeException('You need to disable the view annotations in SensioFrameworkExtraBundle when using the FOSRestBundle View Response listener.');
+            throw new \RuntimeException('You need to disable the view annotations in SensioFrameworkExtraBundle when using the FOSRestBundle View Response listener. Add "view: { annotations: false }" to the sensio_framework_extra: section of your config.yml');
         }
 
         if ($container->has('fos_rest.converter.request_body') && !$container->has('sensio_framework_extra.converter.listener')) {
