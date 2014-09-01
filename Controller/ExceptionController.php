FriendsOfSymfony/FOSRestBundle

commit cf2a86b9c01ffc290118063d6a03cc1c1c4e7d7b

Fixed CS

Controller/ExceptionController.php
@@ -71,14 +71,14 @@ class ExceptionController extends ContainerAware
                 'ExceptionController::showAction can only accept some exceptions (%s, %s), "%s" given',
                 "Symfony\Component\HttpKernel\Exception\FlattenException",
                 "Symfony\Component\Debug\Exception\FlattenException",
-                get_class($exception)
+                get_class($exception),
             ));
         }
 
         $format = $this->getFormat($request, $format);
         if (null === $format) {
             $message = 'No matching accepted Response format could be determined, while handling: ';
-            $message.= $this->getExceptionMessage($exception);
+            $message .= $this->getExceptionMessage($exception);
 
             return new Response($message, Codes::HTTP_NOT_ACCEPTABLE, $exception->getHeaders());
         }
@@ -103,7 +103,7 @@ class ExceptionController extends ContainerAware
             $response = $viewHandler->handle($view);
         } catch (\Exception $e) {
             $message = 'An Exception was thrown while handling: ';
-            $message.= $this->getExceptionMessage($exception);
+            $message .= $this->getExceptionMessage($exception);
             $response = new Response($message, Codes::HTTP_INTERNAL_SERVER_ERROR, $exception->getHeaders());
         }
 
@@ -157,7 +157,7 @@ class ExceptionController extends ContainerAware
             }
         } catch (\ReflectionException $re) {
             return "FOSUserBundle: Invalid class in  fos_res.exception.messages: "
-                    . $re->getMessage();
+                    .$re->getMessage();
         }
 
         return false;

DependencyInjection/Compiler/FormatListenerRulesPass.php
@@ -30,7 +30,7 @@ class FormatListenerRulesPass implements CompilerPassInterface
         if ($container->hasParameter('web_profiler.debug_toolbar.mode')) {
             $path = '_profiler';
             if (2 === $container->getParameter('web_profiler.debug_toolbar.mode')) {
-                $path.= '|_wdt';
+                $path .= '|_wdt';
             }
 
             $profilerRule = array(
@@ -39,7 +39,7 @@ class FormatListenerRulesPass implements CompilerPassInterface
                 'path' => "^/$path/",
                 'priorities' => array('html', 'json'),
                 'fallback_format' => 'html',
-                'prefer_extension' => true
+                'prefer_extension' => true,
             );
 
             $this->addRule($profilerRule, $container);

EventListener/AccessDeniedListener.php
@@ -35,8 +35,8 @@ class AccessDeniedListener extends ExceptionListener
     /**
      * Constructor.
      *
-     * @param array           $formats   An array with keys corresponding to request formats or content types
-     *                                   that must be processed by this listener
+     * @param array           $formats    An array with keys corresponding to request formats or content types
+     *                                    that must be processed by this listener
      * @param string          $challenge
      * @param string          $controller
      * @param LoggerInterface $logger

EventListener/BodyListener.php
@@ -104,7 +104,7 @@ class BodyListener
                     }
                     $request->request = new ParameterBag($data);
                 } else {
-                    throw new BadRequestHttpException('Invalid ' . $format . ' message received');
+                    throw new BadRequestHttpException('Invalid '.$format.' message received');
                 }
             }
         }

Request/AbstractRequestBodyParamConverter.php
@@ -42,8 +42,8 @@ abstract class AbstractRequestBodyParamConverter implements ParamConverterInterf
 
     /**
      * @param object             $serializer
-     * @param array|null         $groups     An array of groups to be used in the serialization context
-     * @param string|null        $version    A version string to be used in the serialization context
+     * @param array|null         $groups                   An array of groups to be used in the serialization context
+     * @param string|null        $version                  A version string to be used in the serialization context
      * @param object             $serializer
      * @param ValidatorInterface $validator
      * @param string|null        $validationErrorsArgument

Request/ParamFetcher.php
@@ -71,7 +71,7 @@ class ParamFetcher implements ParamFetcherInterface
      *
      * Note that adding a param that has the same name as an existing param will override that param.
      *
-     * @param Param                       $param
+     * @param Param $param
      */
     public function addParam(Param $param)
     {
@@ -207,7 +207,7 @@ class ParamFetcher implements ParamFetcherInterface
                     $paramType,
                     $param,
                     $config->requirements
-                )
+                ),
             ));
         }
 

Request/ParamReader.php
@@ -46,6 +46,7 @@ class ParamReader implements ParamReaderInterface
 
         $methodParams = $this->getParamsFromMethod($reflection->getMethod($method));
         $classParams = $this->getParamsFromClass($reflection);
+
         return array_merge($methodParams, $classParams);
     }
 

Request/ParamReaderInterface.php
@@ -37,11 +37,11 @@ interface ParamReaderInterface
      * @return array Param annotation objects of the method. Indexed by parameter name.
      */
     public function getParamsFromMethod(\ReflectionMethod $method);
-    
+
     /**
-     * 
+     *
      * @param \ReflectionClass $class
-     * 
+     *
      * @return array Param annotation objects of the class. Indexed by parameter name.
      */
     public function getParamsFromClass(\ReflectionClass $class);

commit 64942ab1ad56adeae6318a2c372160758cff345c

Fixed bug introduced in cf2a86b

Controller/ExceptionController.php
@@ -69,9 +69,9 @@ class ExceptionController extends ContainerAware
         if (!$exception instanceof DebugFlattenException && !$exception instanceof HttpFlattenException) {
             throw new \InvalidArgumentException(sprintf(
                 'ExceptionController::showAction can only accept some exceptions (%s, %s), "%s" given',
-                "Symfony\Component\HttpKernel\Exception\FlattenException",
-                "Symfony\Component\Debug\Exception\FlattenException",
-                get_class($exception),
+                'Symfony\Component\HttpKernel\Exception\FlattenException',
+                'Symfony\Component\Debug\Exception\FlattenException',
+                get_class($exception)
             ));
         }
 