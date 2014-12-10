c2fe7c0704b643c0f954c0bf058ee630264426ad
Maxime Raoust <maximeraoust@gmail.com>
2014-12-10 17:46:11 +0100

    Fixed handling of fos_rest.body_listener.default_format config parameter.

--- DependencyInjection/FOSRestExtension.php
+++ DependencyInjection/FOSRestExtension.php
@@ -156,6 +156,7 @@ class FOSRestExtension extends Extension implements PrependExtensionInterface
             $loader->load('body_listener.xml');
 
             $container->setParameter($this->getAlias().'.throw_exception_on_unsupported_content_type', $config['body_listener']['throw_exception_on_unsupported_content_type']);
+            $container->setParameter($this->getAlias().'.body_default_format', $config['body_listener']['default_format']);
             $container->setParameter($this->getAlias().'.decoders', $config['body_listener']['decoders']);
 
             $arrayNormalizer = $config['body_listener']['array_normalizer'];
