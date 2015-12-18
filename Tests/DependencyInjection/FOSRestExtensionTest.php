84526d0001db5b7afbc231b5a4d6a4255db46a3d
Ruud Kamphuis <ruudk@mphuis.com>
2015-12-18 11:59:21 +0100

    Added test

--- Tests/DependencyInjection/FOSRestExtensionTest.php
+++ Tests/DependencyInjection/FOSRestExtensionTest.php
@@ -309,6 +309,18 @@ class FOSRestExtensionTest extends \PHPUnit_Framework_TestCase
         $this->extension->load($config, $this->container);
         $this->assertFalse($this->container->has('fos_rest.validator'));
     }
+    
+    public function testBodyConvertorDisabledAndSerializerVersionGiven()
+    {
+        $config = ['fos_rest' => ['body_converter' => ['enabled' => false], 'serializer' => ['version' => '1.0']]];
+        $this->extension->load($config, $this->container);
+    }
+
+    public function testBodyConvertorDisabledAndSerializerGroupsGiven()
+    {
+        $config = ['fos_rest' => ['body_converter' => ['enabled' => false], 'serializer' => ['groups' => ['Default']]]];
+        $this->extension->load($config, $this->container);
+    }
 
     /**
      * Test that extension loads properly.
