abfe1c058376423b2c0e0070b84490bc6fb9eee8
Christian Flothmann <christian.flothmann@xabbuh.de>
2015-12-29 11:53:19 +0100

    introduce a base TemplatingExceptionController

--- Controller/TwigExceptionController.php
+++ Controller/TwigExceptionController.php
@@ -11,40 +11,19 @@
 
 namespace FOS\RestBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Bundle\FrameworkBundle\Templating\TemplateReference;
 use Symfony\Component\Debug\Exception\FlattenException;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
 
 /**
  * Custom ExceptionController that uses the view layer and supports HTTP response status code mapping.
  * It additionally is able to prepare the template parameters for the core EngineInterface.
  */
-class TwigExceptionController extends ExceptionController
+class TwigExceptionController extends TemplatingExceptionController
 {
     /**
-     * @var EngineInterface
-     */
-    private $templating;
-
-    public function setTemplating(EngineInterface $templating)
-    {
-        $this->templating = $templating;
-    }
-
-    public function getTemplating()
-    {
-        if (!$this->templating instanceof EngineInterface) {
-            throw new \RuntimeException('No templating engine set');
-        }
-
-        return $this->templating;
-    }
-
-    /**
-     * {inheritDoc}.
+     * {@inheritdoc}
      */
     protected function createView($format, FlattenException $exception, $code, $parameters, Request $request, $showException)
     {
@@ -59,7 +38,7 @@ class TwigExceptionController extends ExceptionController
     }
 
     /**
-     * {inheritDoc}.
+     * {@inheritdoc}
      */
     protected function getParameters($currentContent, $code, $exception, DebugLoggerInterface $logger = null, $format = 'html')
     {
@@ -73,20 +52,10 @@ class TwigExceptionController extends ExceptionController
     }
 
     /**
-     * Finds the template for the given format and status code.
-     *
-     * Note this method needs to be overridden in case another
-     * engine than Twig should be supported;
+     * {@inheritdoc}
      *
      * This code is inspired by TwigBundle and should be synchronized on a regular basis
      * see src/Symfony/Bundle/TwigBundle/Controller/ExceptionController.php
-     *
-     * @param Request $request
-     * @param string  $format
-     * @param int     $statusCode
-     * @param bool    $showException
-     *
-     * @return TemplateReference
      */
     protected function findTemplate(Request $request, $format, $statusCode, $showException)
     {
@@ -98,14 +67,14 @@ class TwigExceptionController extends ExceptionController
         // when not in debug, try to find a template for the specific HTTP status code and format
         if (!$showException) {
             $template = new TemplateReference('TwigBundle', 'Exception', $name.$statusCode, $format, 'twig');
-            if ($this->getTemplating()->exists($template)) {
+            if ($this->templating->exists($template)) {
                 return $template;
             }
         }
 
         // try to find a template for the given format
         $template = new TemplateReference('TwigBundle', 'Exception', $name, $format, 'twig');
-        if ($this->getTemplating()->exists($template)) {
+        if ($this->templating->exists($template)) {
             return $template;
         }
 
