8f9590d57591e8cfd372de42d865c7e9bd41f7fe
Maximilian Bosch <maximilian.bosch.27@gmail.com>
2016-01-15 19:21:50 +0100

    tests with the annotation controller

--- Tests/Fixtures/Controller/AnnotatedUsersController.php
+++ Tests/Fixtures/Controller/AnnotatedUsersController.php
@@ -11,17 +11,24 @@
 
 namespace FOS\RestBundle\Tests\Fixtures\Controller;
 
+use FOS\RestBundle\Controller\Annotations\Copy;
 use FOS\RestBundle\Controller\Annotations\Delete;
 use FOS\RestBundle\Controller\Annotations\Get;
 use FOS\RestBundle\Controller\Annotations\Head;
 use FOS\RestBundle\Controller\Annotations\Link;
+use FOS\RestBundle\Controller\Annotations\Lock;
+use FOS\RestBundle\Controller\Annotations\Mkcol;
+use FOS\RestBundle\Controller\Annotations\Move;
 use FOS\RestBundle\Controller\Annotations\NoRoute;
 use FOS\RestBundle\Controller\Annotations\Options;
 use FOS\RestBundle\Controller\Annotations\Patch;
 use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\PropFind;
+use FOS\RestBundle\Controller\Annotations\PropPatch;
 use FOS\RestBundle\Controller\Annotations\Put;
 use FOS\RestBundle\Controller\Annotations\Route;
 use FOS\RestBundle\Controller\Annotations\Unlink;
+use FOS\RestBundle\Controller\Annotations\Unlock;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AnnotatedUsersController extends Controller
@@ -30,6 +37,83 @@ class AnnotatedUsersController extends Controller
      * [OPTIONS]     /users.
      */
     public function optionsUsersAction()
+    {
+    }
+
+    /**
+     * [COPY] /users/{id}.
+     *
+     * @param $id
+     *
+     * @Copy()
+     */
+    public function copyUserAction($id)
+    {
+    }
+
+    /**
+     * [PROPFIND] /users/{id}/props/{property}.
+     *
+     * @param $id
+     * @param $property
+     *
+     * @PropFind()
+     */
+    public function propfindUserPropsAction($id, $property)
+    {
+    }
+
+    /**
+     * [PROPPATCH] /users/{id}/props/{property}.
+     *
+     * @param $id
+     * @param $property
+     *
+     * @PropPatch()
+     */
+    public function proppatchUserPropsAction($id, $property)
+    {
+    }
+
+    /**
+     * [MOVE] /users/{id}.
+     *
+     * @param $id
+     *
+     * @Move()
+     */
+    public function moveUserAction($id)
+    {
+    }
+
+    /**
+     * [MKCOL] /users.
+     *
+     * @Mkcol()
+     */
+    public function mkcolUsersAction()
+    {
+    }
+
+    /**
+     * [LOCK] /users/{slug}.
+     *
+     * @param $slug
+     *
+     * @Lock()
+     */
+    public function lockUserAction($slug)
+    {
+    }
+
+    /**
+     * [UNLOCK] /users/{slug}.
+     *
+     * @param $slug
+     *
+     * @Unlock()
+     */
+    public function unlockUserAction($slug)
     {
     }
 
