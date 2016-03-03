977e148c5f4775d40bc2868c6ec39255c5c4db3f
Ener-Getick <egetick@gmail.com>
2016-03-03 18:27:08 +0100

    Allow to customize deeper the AccessDeniedListener

--- EventListener/AccessDeniedListener.php
+++ EventListener/AccessDeniedListener.php
@@ -12,15 +12,14 @@
 namespace FOS\RestBundle\EventListener;
 
 use FOS\RestBundle\FOSRestBundle;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
-use Symfony\Component\HttpKernel\EventListener\ExceptionListener;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
  * This listener handles ensures that for specific formats AccessDeniedExceptions
@@ -30,7 +29,7 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
  *
  * @internal
  */
-class AccessDeniedListener extends ExceptionListener
+class AccessDeniedListener implements EventSubscriberInterface
 {
     private $formats;
     private $challenge;
@@ -38,17 +37,14 @@ class AccessDeniedListener extends ExceptionListener
     /**
      * Constructor.
      *
-     * @param array           $formats    An array with keys corresponding to request formats or content types
-     *                                    that must be processed by this listener
-     * @param string          $challenge
-     * @param string          $controller
-     * @param LoggerInterface $logger
+     * @param array  $formats   An array with keys corresponding to request formats or content types
+     *                          that must be processed by this listener
+     * @param string $challenge
      */
-    public function __construct($formats, $challenge, $controller, LoggerInterface $logger = null)
+    public function __construct($formats, $challenge)
     {
         $this->formats = $formats;
         $this->challenge = $challenge;
-        parent::__construct($controller, $logger);
     }
 
     public function onKernelException(GetResponseForExceptionEvent $event)
@@ -76,7 +72,6 @@ class AccessDeniedListener extends ExceptionListener
         if ($exception instanceof AccessDeniedException) {
             $exception = new AccessDeniedHttpException('You do not have the necessary permissions', $exception);
             $event->setException($exception);
-            parent::onKernelException($event);
         } elseif ($exception instanceof AuthenticationException) {
             if ($this->challenge) {
                 $exception = new UnauthorizedHttpException($this->challenge, 'You are not authenticated', $exception);
@@ -84,7 +79,6 @@ class AccessDeniedListener extends ExceptionListener
                 $exception = new HttpException(401, 'You are not authenticated', $exception);
             }
             $event->setException($exception);
-            parent::onKernelException($event);
         }
 
         $handling = false;
