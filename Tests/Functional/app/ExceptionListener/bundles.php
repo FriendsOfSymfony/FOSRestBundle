c1e42be9583f2f57516c814a23f820ea84e5abca
Christian Flothmann <christian.flothmann@xabbuh.de>
2016-03-31 22:14:35 +0200

    add test for previous commit

---
+++ Tests/Functional/app/ExceptionListener/bundles.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the FOSRestBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+    new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+    new \Symfony\Bundle\TwigBundle\TwigBundle(),
+    new \FOS\RestBundle\FOSRestBundle(),
+    new \FOS\RestBundle\Tests\Functional\Bundle\TestBundle\TestBundle(),
+];
