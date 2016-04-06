932c7f94ed0d9a090b667e0f60b0cbc31bbe9b70
Christian Flothmann <christian.flothmann@gmail.com>
2016-04-06 21:43:24 +0200

    merge branch 1.7 into 1.8

---
+++ Tests/Functional/AllowedMethodsTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the FOSRestBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\RestBundle\Tests\Functional;
+
+/**
+ * @author Ener-Getick <egetick@gmail.com>
+ */
+class AllowedMethodsTest extends WebTestCase
+{
+    public function testAllowHeader()
+    {
+        $client = $this->createClient(array('test_case' => 'AllowedMethodsListener'));
+        $client->request('POST', '/allowed-methods');
+        $this->assertEquals('GET, LOCK, POST, PUT', $client->getResponse()->headers->get('Allow'));
+    }
+}
