f0d47ec92d4378142c12961e411a2f475e59c58d
Christian Flothmann <christian.flothmann@xabbuh.de>
2016-05-08 09:17:24 +0200

    deprecate RedirectView and RouteRedirectView

--- View/RouteRedirectView.php
+++ View/RouteRedirectView.php
@@ -11,6 +11,8 @@
 
 namespace FOS\RestBundle\View;
 
+@trigger_error(sprintf('The %s\RouteRedirectView class is deprecated since version 1.8 and will be removed in 2.0. Use %s\View::createRouteRedirect() instead.', __NAMESPACE__, __NAMESPACE__), E_USER_DEPRECATED);
+
 use FOS\RestBundle\Util\Codes;
 
 /**
@@ -18,6 +20,8 @@ use FOS\RestBundle\Util\Codes;
  *
  * @author Johannes M. Schmitt <schmittjoh@gmail.com>
  * @author Lukas K. Smith <smith@pooteeweet.org>
+ *
+ * @deprecated since 1.8, to be removed in 2.0. Use View::createRouteRedirect() instead.
  */
 class RouteRedirectView
 {
