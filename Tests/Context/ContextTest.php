4b3a8c4da058992fb22d6eed03e90b03038fe8ef
bblue <aleksander.lanes@gmail.com>
2016-10-08 21:25:09 +0200

    Added test for exclusion strategies

--- Tests/Context/ContextTest.php
+++ Tests/Context/ContextTest.php
@@ -98,4 +98,20 @@ class ContextTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals(true, $this->context->getSerializeNull());
     }
+
+    public function testExclusionStrategy()
+    {
+        $strategy1 = $this->getMock('JMS\Serializer\Exclusion\ExclusionStrategyInterface');
+        $strategy2 = $this->getMock('JMS\Serializer\Exclusion\ExclusionStrategyInterface');
+
+        $this->context->addExclusionStrategy($strategy1);
+        $this->context->addExclusionStrategy($strategy2);
+
+        $strategies = $this->context->getExclusionStrategies();
+
+        $this->assertEquals(true, is_array($strategies));
+        $this->assertEquals(true, !empty($strategies));
+        $this->assertEquals(true, in_array($strategy1, $strategies, true));
+        $this->assertEquals(true, in_array($strategy2, $strategies, true));
+    }
 }
