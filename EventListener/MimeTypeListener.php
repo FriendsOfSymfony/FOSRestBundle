a5670d3ae44c9d84418b4883cba5bdfa93b09dbf
Abdessamad Idrissi <numediaweb@gmail.com>
2016-11-28 16:29:15 +0100

    fixes #1617
    Check if the `$format` exists (is an array) before merging into `$mimeTypes`.

--- EventListener/MimeTypeListener.php
+++ EventListener/MimeTypeListener.php
@@ -54,7 +54,8 @@ class MimeTypeListener
         if (HttpKernelInterface::MASTER_REQUEST === $event->getRequestType()) {
             foreach ($this->mimeTypes as $format => $mimeTypes) {
                 if (method_exists(Request::class, 'getMimeTypes')) {
-                    $mimeTypes = array_merge($mimeTypes, Request::getMimeTypes($format));
+                    $mimeTypesForFormat = Request::getMimeTypes($format);
+                    $mimeTypes = $mimeTypesForFormat ? array_merge($mimeTypes,$mimeTypesForFormat) : $mimeTypes;
                 } elseif (null !== $request->getMimeType($format)) {
                     $class = new \ReflectionClass(Request::class);
                     $properties = $class->getStaticProperties();
