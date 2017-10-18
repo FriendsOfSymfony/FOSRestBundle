8c0d01de9d6924d64dab15d9f76116cb707bc581
Peter Culka <p.culka@gmail.com>
2017-10-18 21:33:32 +0200

    switched bitwise XOR to logical OR using PHP5.5+ class declaration added check for RequestBodyParamConverter

--- DependencyInjection/Compiler/ConfigurationCheckPass.php
+++ DependencyInjection/Compiler/ConfigurationCheckPass.php
@@ -13,6 +13,8 @@ namespace FOS\RestBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Sensio\Bundle\FrameworkExtraBundle\EventListener\TemplateListener;
+use Sensio\Bundle\FrameworkExtraBundle\EventListener\ParamConverterListener;
 
 /**
  * Checks if the SensioFrameworkExtraBundle views annotations are disabled when using the View Response listener.
@@ -25,12 +27,12 @@ final class ConfigurationCheckPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        if ($container->has('fos_rest.converter.request_body') && !$container->has('sensio_framework_extra.converter.listener')) {
+        if ($container->has('fos_rest.converter.request_body') && !($container->has('sensio_framework_extra.converter.listener') || $container->hasDefinition(ParamConverterListener::class))) {
             throw new \RuntimeException('You need to enable the parameter converter listeners in SensioFrameworkExtraBundle when using the FOSRestBundle RequestBodyParamConverter');
         }
 
         if ($container->has('fos_rest.view_response_listener') && isset($container->getParameter('kernel.bundles')['SensioFrameworkExtraBundle'])) {
-            if (!($container->has('sensio_framework_extra.view.listener') ^ $container->hasDefinition('Sensio\Bundle\FrameworkExtraBundle\EventListener\TemplateListener'))) {
+            if (!($container->has('sensio_framework_extra.view.listener') || $container->hasDefinition(TemplateListener::class))) {
                 throw new \RuntimeException('You must enable the SensioFrameworkExtraBundle view annotations to use the ViewResponseListener.');
             }
         }
