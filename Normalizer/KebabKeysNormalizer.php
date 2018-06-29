ed63467260a2fe1e7e5c50a5a14a9079f979e507
Oleg Andreyev <oleg@andreyev.lv>
2018-06-29 22:22:04 +0300

    adding kebak-case key normalizer

---
+++ Normalizer/KebabKeysNormalizer.php
@@ -0,0 +1,84 @@
+<?php
+/*
+ * This file is part of the FOSRestBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\RestBundle\Normalizer;
+
+use FOS\RestBundle\Normalizer\Exception\NormalizationException;
+
+/**
+ * Normalizes the array by changing its keys from kebab case to camel case.
+ *
+ * @author Oleg Andreyev <oleg.andreyev@live.com>
+ */
+class KebabKeysNormalizer implements ArrayNormalizerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize(array $data)
+    {
+        $this->normalizeArray($data);
+
+        return $data;
+    }
+
+    /**
+     * Normalizes an array.
+     *
+     * @param array &$data
+     *
+     * @throws Exception\NormalizationException
+     */
+    private function normalizeArray(array &$data)
+    {
+        $normalizedData = array();
+
+        foreach ($data as $key => $val) {
+            $normalizedKey = $this->normalizeString($key);
+
+            if ($normalizedKey !== $key) {
+                if (array_key_exists($normalizedKey, $normalizedData)) {
+                    throw new NormalizationException(sprintf(
+                        'The key "%s" is invalid as it will override the existing key "%s"',
+                        $key,
+                        $normalizedKey
+                    ));
+                }
+            }
+
+            $normalizedData[$normalizedKey] = $val;
+            $key = $normalizedKey;
+
+            if (is_array($val)) {
+                $this->normalizeArray($normalizedData[$key]);
+            }
+        }
+
+        $data = $normalizedData;
+    }
+
+    /**
+     * Normalizes a string.
+     *
+     * @param string $string
+     *
+     * @return string
+     */
+    protected function normalizeString($string)
+    {
+        if (false === strpos($string, '-')) {
+            return $string;
+        }
+
+        return \preg_replace_callback('/-([a-zA-Z0-9])/', function ($matches) {
+            return strtoupper($matches[1]);
+        }, $string);
+    }
+}
---
+++ Tests/Normalizer/KebabKeysNormalizerTest.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the FOSRestBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace FOS\RestBundle\Tests\Normalizer;
+
+use FOS\RestBundle\Normalizer\KebabKeysNormalizer;
+use PHPUnit\Framework\TestCase;
+
+class KebabKeysNormalizerTest extends TestCase
+{
+    /**
+     * @expectedException \FOS\RestBundle\Normalizer\Exception\NormalizationException
+     */
+    public function testNormalizeSameValueException()
+    {
+        $normalizer = new KebabKeysNormalizer();
+        $normalizer->normalize([
+            'foo' => [
+                'foo-bar' => 'foo',
+                'foo-Bar' => 'foo',
+            ],
+        ]);
+    }
+
+    /**
+     * @dataProvider normalizeProvider
+     */
+    public function testNormalize(array $array, array $expected)
+    {
+        $normalizer = new KebabKeysNormalizer();
+        $this->assertEquals($expected, $normalizer->normalize($array));
+    }
+
+    public function normalizeProvider()
+    {
+        $array = $this->normalizeProviderCommon();
+        $array[] = array(
+            array('_-username' => 'foo', '-password' => 'bar', '-foo-bar' => 'foobar'),
+            array('_Username' => 'foo', 'Password' => 'bar', 'FooBar' => 'foobar'),
+        );
+
+        return $array;
+    }
+
+    private function normalizeProviderCommon()
+    {
+        return array(
+            array(array(), array()),
+            array(
+                array('foo' => array('Foo-bar-baz' => array('foo-Bar' => array('foo-bar' => 'foo_bar'))),
+                    'foo-1ar' => array('foo_bar'),
+                ),
+                array('foo' => array('FooBarBaz' => array('fooBar' => array('fooBar' => 'foo_bar'))),
+                    'foo1ar' => array('foo_bar'),
+                ),
+            ),
+        );
+    }
+}
