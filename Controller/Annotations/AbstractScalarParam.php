d6e1b38f71399b48d9d9e76e576b1695969b795d
Gonzalo1987 <Gonzalo1987@users.noreply.github.com>
2019-07-24 13:40:21 +0200

    Update AbstractScalarParam.php
    Fixed #1496

--- Controller/Annotations/AbstractScalarParam.php
+++ Controller/Annotations/AbstractScalarParam.php
@@ -65,7 +65,9 @@ abstract class AbstractScalarParam extends AbstractParam
         }
 
         if (false === $this->allowBlank) {
-            $constraints[] = new NotBlank();
+            $notBlank = new NotBlank();
+            $notBlank->allowNull = $this->nullable;
+            $constraints[] = $notBlank;
         }
 
         // If the user wants to map the value, apply all constraints to every
