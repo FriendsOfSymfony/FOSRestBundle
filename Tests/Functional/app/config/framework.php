a31f01c21e5276a181668f56eddc44559630b9d9
W0rma <beck.worma@gmail.com>
2021-08-10 08:56:43 +0200

    Fix session storage deprecation when using symfony > 5.2

---
+++ Tests/Functional/app/config/framework.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the FOSRestBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// BC layer for symfony < 5.3
+if (class_exists(\Symfony\Component\HttpFoundation\Session\SessionFactory::class)) {
+    $sessionConfig = [
+        'storage_factory_id' => 'session.storage.factory.mock_file',
+    ];
+} else {
+    $sessionConfig = [
+        'storage_id' => 'session.storage.mock_file',
+    ];
+}
+
+$container->loadFromExtension('framework', [
+    'annotations' => [
+        'enabled' => true,
+    ],
+    'property_access' => null,
+    'secret' => 'test',
+    'router' => [
+        'resource' => "%kernel.project_dir%/config/routing.yml",
+        'utf8' => true,
+    ],
+    'test' => null,
+    'csrf_protection' => null,
+    'form' => null,
+    'session' => $sessionConfig,
+    'default_locale' => 'en',
+]);
+
