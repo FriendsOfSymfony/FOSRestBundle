1a4f1b6d03a8f9f959a8af8365f94534e930044b
W0rma <beck.worma@gmail.com>
2021-08-16 16:55:35 +0200

    Make annotations extending Route annotation compatible with symfony/routing > 5.1 (#2312)

---
+++ Tests/Controller/Annotations/RouteTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace FOS\RestBundle\Tests\Controller\Annotations;
+
+use FOS\RestBundle\Controller\Annotations\Route;
+use PHPUnit\Framework\TestCase;
+
+class RouteTest extends TestCase
+{
+    public function testCanInstantiate()
+    {
+        $path = '/path';
+        $name = 'route_name';
+        $requirements = ['locale' => 'en'];
+        $options = ['compiler_class' => 'RouteCompiler'];
+        $defaults = ['_controller' => 'MyBlogBundle:Blog:index'];
+        $host = '{locale}.example.com';
+        $methods = ['GET', 'POST'];
+        $schemes = ['https'];
+        $condition = 'context.getMethod() == "GET"';
+
+        $route = new Route(
+            $path,
+            null,
+            $name,
+            $requirements,
+            $options,
+            $defaults,
+            $host,
+            $methods,
+            $schemes,
+            $condition
+        );
+
+        $this->assertEquals($path, $route->getPath());
+        $this->assertEquals($name, $route->getName());
+        $this->assertEquals($requirements, $route->getRequirements());
+        $this->assertEquals($options, $route->getOptions());
+        $this->assertEquals($defaults, $route->getDefaults());
+        $this->assertEquals($host, $route->getHost());
+        $this->assertEquals($methods, $route->getMethods());
+        $this->assertEquals($schemes, $route->getSchemes());
+        $this->assertEquals($condition, $route->getCondition());
+    }
+}
