aa383604c6717c24bb9d787f3132edc4c2df1594
Asmir Mustafic <goetas@gmail.com>
2021-10-09 23:25:57 +0200

    do not decorate the jms handler registry for jms v4

--- DependencyInjection/Compiler/HandlerRegistryDecorationPass.php
+++ DependencyInjection/Compiler/HandlerRegistryDecorationPass.php
@@ -33,6 +33,7 @@ class HandlerRegistryDecorationPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
     {
+        // skip if JMSSerializerBundle is not installed or if JMSSerializerBundle >= 4.0
         if (!$container->has('fos_rest.serializer.jms_handler_registry') || $container->has('jms_serializer.handler_registry.service_locator')) {
             return;
         }
